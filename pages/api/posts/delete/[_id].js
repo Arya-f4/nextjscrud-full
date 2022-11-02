0d86811fbd6677412df9ba9d8526473453e40f39
WorldSavior <45348306+kajuki27@users.noreply.github.com>
2022-11-02 18:40:39 +0700

    Update [_id].js

--- pages/api/posts/delete/[_id].js
+++ pages/api/posts/delete/[_id].js
@@ -7,9 +7,9 @@ export default async function handler(req, res) {
   if (req.method !== 'DELETE') return res.status(405).end();
   await connectMongo();
   const { _id } = req.query;
-  const idpure = _id.slice(0, -2);
+
   const auth = await authorization(req, res);
-  const deleteRow = await Posts.findOneAndDelete({ idpure });
+  const deleteRow = await Posts.findOneAndDelete({ _id });
 
   res.status(200);
   res.json({
