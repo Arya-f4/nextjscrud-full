34136ffe0af44039a6d5c4c49b8c9c96a8c3101b
WorldSavior <45348306+kajuki27@users.noreply.github.com>
2022-11-02 18:36:10 +0700

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -3,19 +3,9 @@ import Cookie from 'js-cookie';
 import Router from 'next/router';
 import { unauthPage } from "../../middlewares/authorizationPage";
 import Link from "next/link";
-import Head from "next/head";
-import theme from '../../utils/theme';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export async function getServerSideProps(ctx) {
 
