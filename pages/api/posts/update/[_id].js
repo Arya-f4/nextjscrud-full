b7da79c851cde31775d1a518e4cb0c70874b28d3
WorldSavior <45348306+kajuki27@users.noreply.github.com>
2022-11-02 18:41:39 +0700

    Update [_id].js

--- pages/api/posts/update/[_id].js
+++ pages/api/posts/update/[_id].js
@@ -1,15 +1,12 @@
-import db from '../../../../libs/db';
-import authorization from '../../../../middlewares/authorization';
+
 import Posts from '../../../../models/postModel';
 
 export default async function handler(req, res) {
   if (req.method !== 'PUT') return res.status(405).end();
-  const auth = await authorization(req, res);
 
   const { _id } = req.query;
-  const idpure = _id.slice(0, -2);
   const { nama_manusia, no_hp, alamat, email } = req.body;
-  const update = await Posts.findOneAndUpdate({ idpure }, { nama_manusia, no_hp, alamat, email });
+  const update = await Posts.findOneAndUpdate({ _id }, { nama_manusia, no_hp, alamat, email });
 
   const updatedData = await Posts.findOne({ _id });
   res.status(200);
