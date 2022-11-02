63925581f25028c1abaaf6ec80b15af37f9b82da
WorldSavior <45348306+kajuki27@users.noreply.github.com>
2022-11-02 17:03:38 +0700

    Update [_id].js
    wrong line request and respond placement

--- pages/api/posts/detail/[_id].js
+++ pages/api/posts/detail/[_id].js
@@ -5,11 +5,12 @@ import Posts from '../../../../models/postModel';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end();
+  const auth = await authorization(req, res);
   await connectMongo();
 
   const { _id } = req.query;
   const idpure = _id.slice(0, -2);
-  const auth = await authorization(req, res);
+ 
 
   const posts = await Posts.findOne({ idpure });
 
