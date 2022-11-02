901f969a79dd28bfae555fbc6ea65ad12b8d5d80
WorldSavior <45348306+kajuki27@users.noreply.github.com>
2022-11-02 17:04:50 +0700

    Update [_id].js

--- pages/api/posts/detail/[_id].js
+++ pages/api/posts/detail/[_id].js
@@ -1,12 +1,11 @@
-import db from '../../../../libs/db';
-import connectMongo from '../../../../libs/db';
+
 import authorization from '../../../../middlewares/authorization';
 import Posts from '../../../../models/postModel';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end();
   const auth = await authorization(req, res);
-  await connectMongo();
+  
 
   const { _id } = req.query;
   const idpure = _id.slice(0, -2);
